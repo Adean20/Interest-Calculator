6f4c0e64e1f06b6b59048d160f345d57a7362830
Austin Dean <austindean3@hotmail.com>
2021-01-16 21:01:18 -0500

    Complete overhaul. Seems to work.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+		//Create variables from input
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
     
+    //do math
+    let interest = principal * years * rate / 100;
+    let endYear = new Date().getFullYear() + parseInt(years);
+		
+    //validate the principal. If negative or 0 alert user and focus on principal
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    
+    //display result
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest}</mark>,<br>in the year <mark>${endYear}</mark><br>`;
+}
+
+//display rate % by slider
+function rangeReader()
+{
+    let sliderValue = document.getElementById("rate").value;
+    document.getElementById("sliderValue").innerText = `${sliderValue}%`;
 }
         
